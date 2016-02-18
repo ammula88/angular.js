e616eb70193ffd7db0c98c93f78db336f99cc92b
Anil Ammula <aa654@njit.edu>
2016-02-18 08:05:46 -0500

    Branch: Master HEAD:18a5e41f6d9c7e0f2f7d86ef469eb2c7c1b6bad0 Nonce: 1631710076

--- test.sh
+++
@@ -1,7 +0,0 @@
-tests=$1
-if [[ $tests = "" ]]; then
-  tests="all"
-fi
-
-java -jar lib/jstestdriver/JsTestDriver.jar --tests "$tests"
-#java -jar lib/jstestdriver/JsTestDriver.jar --tests "$tests" --config jsTestDriver-jquery.conf
--- test/jquery_remove.js
+++
@@ -1,1 +0,0 @@
-var _jQuery = jQuery.noConflict(true);
